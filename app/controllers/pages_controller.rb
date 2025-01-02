5104979420d17f2572c9f38200d871efc2aef35b
carvalhio <raffael.carvallho@gmail.com>
2025-01-02 10:37:21 -0300

    views upgrade

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,49 @@
+﻿class PagesController < ApplicationController
+  def home
+  end
+    def exams
+    @stages = ['Etapa 1', 'Etapa 2', 'Etapa 3', 'Etapa 4']
+  end
+
+  def stages
+    @stage = params[:stage]
+    @types = ['Parcial', 'Global']
+  end
+
+  def types
+    @stage = params[:stage]
+    @type = params[:type]
+    @levels = ['Anos Iniciais', 'Anos Finais', 'Ensino Médio']
+  end
+
+  def levels
+    @stage = params[:stage]
+    @type = params[:type]
+    @level = params[:level]
+    @dates = ['2024-01-15', '2024-02-20'] # Exemplos
+  end
+
+  def schedule
+    @stage = params[:stage]
+    @type = params[:type]
+    @level = params[:level]
+    @date = params[:date]
+
+    @subjects = [
+      { name: 'Matemática', professor: 'Liliane Alves', shift: 'Manhã', status: 'Pendente' },
+      { name: 'História', professor: 'Júnior Alves', shift: 'Tarde', status: 'Recebido' }
+    ]
+  end
+
+  def manager
+    @stage = params[:stage]
+    @type = params[:type]
+    @level = params[:level]
+    @date = params[:date]
+
+    @tests = [
+      { subject: 'Matemática', professor: 'Liliane Alves', shift: 'Manhã', status: 'Pendente' },
+      { subject: 'História', professor: 'Júnior Alves', shift: 'Tarde', status: 'Recebido' }
+    ]
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  root 'pages#home'
 
-  # Render dynamic PWA files from app/views/pwa/*
-  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # Rotas para exames
+  get 'exams', to: 'pages#exams'
+  get 'exams/stages/:stage', to: 'pages#stages', as: 'exam_stage'
+  get 'exams/stages/:stage/types/:type', to: 'pages#types', as: 'exam_type'
+  get 'exams/stages/:stage/types/:type/levels/:level', to: 'pages#levels', as: 'exam_level'
+  get 'exams/stages/:stage/types/:type/levels/:level/schedule', to: 'pages#schedule', as: 'exam_schedule'
+  get 'exams/stages/:stage/types/:type/levels/:level/schedule/:date', to: 'pages#manager', as: 'exam_manager'
 end
+
